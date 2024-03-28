97bbe34c1f4ab8b8547e6a0c9827c2cf023ed790
KiraKostina <kostina.kira@gmail.com>
2024-03-29 00:24:14 +0200

    update

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,17 +1,21 @@
 // Функція для HTTP-запитів
 const API_KEY = "43048239-bd17c0d5033bfd52479da7a86";
-const BASE_URL = "https://pixabay.com/api";
+const BASE_URL = "https://pixabay.com/api/";
 
-export function getPhotos(q) {
+export function getPhotos(query) {
     const params = {
-        q,
+        q: query,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
-        key: API_KEY,
-    // per_page: 12,
-    // page,    
+        key: API_KEY,   
   };
   const options = new URLSearchParams(params);
-  return fetch(`${BASE_URL}${options}`).then(res => res.json());
+  return fetch(`${BASE_URL}?${options}`).then(res => {
+    if (!res.ok) {
+      throw new Error('Error!');
+    }
+
+    return res.json();
+  });
 }
